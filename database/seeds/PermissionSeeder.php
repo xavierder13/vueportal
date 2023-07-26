7336254f537799f4d88936147790d0b44c4bb612
Xavier De Guzman <47979972+xavierder13@users.noreply.github.com>
2023-07-26 12:11:05 +0800

    Update PermissionSeeder.php

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -174,7 +174,7 @@ class PermissionSeeder extends Seeder
  
  
          foreach ($permissions as $permission) {
-              Permission::create(['name' => $permission]);
+              Permission::firstOrCreate(['name' => $permission]);
          }
     }
 }
