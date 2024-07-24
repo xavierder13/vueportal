57e06425634bacb88536f27985644e829bd8e831
Xavier De Guzman <47979972+xavierder13@users.noreply.github.com>
2024-07-24 14:40:20 +0800

    Update Department.php

--- app/Department.php
+++ app/Department.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    protected $fillable = ['name', 'active'];
+    protected $fillable = ['name', 'division_id', 'active'];
+
+    public function division()
+    {
+        return $this->hasOne('App\Division', 'id', 'division_id');
+        //                 ( <Model>, <id_of_specified_Model>, <id_of_this_model> )
+    }
 }
